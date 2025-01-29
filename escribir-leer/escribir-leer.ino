76a1c7484432289bb653cd62328185ccd3b2a05c
Supertocho80 <aleixdz@gmail.com>
2025-01-29 19:59:10 +0100

    hoy

---
+++ escribir-leer/escribir-leer.ino
@@ -0,0 +1,152 @@
+#include <MFRC522v2.h>
+#include <MFRC522DriverSPI.h>
+//#include <MFRC522DriverI2C.h>
+#include <MFRC522DriverPinSimple.h>
+#include <MFRC522Debug.h>
+
+// Learn more about using SPI/I2C or check the pin assigment for your board: https://github.com/OSSLibraries/Arduino_MFRC522v2#pin-layout
+MFRC522DriverPinSimple ss_pin(5);
+
+MFRC522DriverSPI driver{ss_pin}; // Create SPI driver
+//MFRC522DriverI2C driver{};     // Create I2C driver
+MFRC522 mfrc522{driver};         // Create MFRC522 instance
+
+MFRC522::MIFARE_Key key;
+
+byte blockAddress1 = 10;
+byte blockAddress2 = 9;
+byte newBlockData1[18] = {"roca.marian23"};
+byte newBlockData2[18] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+//byte newBlockData[16] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};   // CLEAR DATA
+byte bufferblocksize = 18;
+byte blockDataRead1[18];
+byte blockDataRead2[18];
+
+void setup() {
+  Serial.begin(115200);  // Initialize serial communication
+  while (!Serial);       // Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4).
+  
+  mfrc522.PCD_Init();    // Init MFRC522 board.
+  Serial.println(F("Warning: this example overwrites a block in your card, use with care!"));
+ 
+  // Prepare key - all keys are set to FFFFFFFFFFFF at chip delivery from the factory.
+  for (byte i = 0; i < 6; i++) {
+    key.keyByte[i] = 0xFF;
+  }
+}
+
+void loop() {
+  // Check if a new card is present
+  if (!mfrc522.PICC_IsNewCardPresent() || !mfrc522.PICC_ReadCardSerial()) {
+    delay(500);
+    return;
+  }
+
+  // Display card UID
+  Serial.print("----------------\nCard UID: ");
+  MFRC522Debug::PrintUID(Serial, (mfrc522.uid));
+  Serial.println();
+
+  // Authenticate the specified block using KEY_A = 0x60
+  if (mfrc522.PCD_Authenticate(0x60, blockAddress1, &key, &(mfrc522.uid)) != 0) {
+    Serial.println("Authentication failed.");
+    return;
+  }
+  
+  // Write data to the specified block
+  if (mfrc522.MIFARE_Write(blockAddress1, newBlockData1, 16) != 0) {
+    Serial.println("Write failed.");
+  } else {
+    Serial.print("Data written successfully in block: ");
+    Serial.println(blockAddress1);
+  }
+
+  // Authenticate the specified block using KEY_A = 0x60
+  if (mfrc522.PCD_Authenticate(0x60, blockAddress2, &key, &(mfrc522.uid)) != 0) {
+    Serial.println("Authentication failed.");
+    return;
+  }
+  
+  // Write data to the specified block
+  if (mfrc522.MIFARE_Write(blockAddress2, newBlockData2, 16) != 0) {
+    Serial.println("Write failed.");
+  } else {
+    Serial.print("Data written successfully in block: ");
+    Serial.println(blockAddress2);
+  }
+
+
+
+
+
+
+  // Authenticate the specified block using KEY_A = 0x60
+  if (mfrc522.PCD_Authenticate(0x60, blockAddress1, &key, &(mfrc522.uid)) != 0) {
+    Serial.println("Authentication failed.");
+    return;
+  }
+
+    // Authenticate the specified block using KEY_A = 0x60
+  if (mfrc522.PCD_Authenticate(0x60, blockAddress2, &key, &(mfrc522.uid)) != 0) {
+    Serial.println("Authentication failed.");
+    return;
+  }
+
+  // Read data from the specified block
+  if (mfrc522.MIFARE_Read(blockAddress1, blockDataRead1 , &bufferblocksize) != 0) {
+    Serial.println("Read failed.");
+  } else {
+    Serial.println("Read successfully!");
+    Serial.print("Data in block ");
+    Serial.print(blockAddress1);
+    Serial.print(": ");
+    for (byte i = 0; i < 16; i++) {
+      Serial.print((char)blockDataRead1[i]);  // Print as character
+    }
+    Serial.println();
+  }
+
+
+
+  if (mfrc522.MIFARE_Read(blockAddress2, blockDataRead2 , &bufferblocksize) != 0) {
+    Serial.println("Read failed.");
+  } else {
+    Serial.println("Read successfully!");
+    Serial.print("Data in block ");
+    Serial.print(blockAddress2);
+    Serial.print(": ");
+    for (byte i = 0; i < 16; i++) {
+      Serial.print((char)blockDataRead2[i]);  // Print as character
+    }
+    Serial.println();
+  }
+    Serial.println();
+  //LEER 1 BLOQUE
+  if (mfrc522.MIFARE_Read(blockAddress1, blockDataRead1 , &bufferblocksize) != 0) {
+    Serial.println("Read failed.");
+  } else {
+    for (byte i = 0; i < 16; i++) {
+      Serial.print((char)blockDataRead1[i]);  // Print as character
+    }
+    
+ 
+  }
+
+  // LEER 2 BLOQUE
+  if (mfrc522.MIFARE_Read(blockAddress2, blockDataRead2 , &bufferblocksize) != 0) {
+    Serial.println("Read failed.");
+  } else {
+    for (byte i = 0; i < 9; i++) {
+      Serial.print((char)blockDataRead2[i]);  // Print as character
+    }
+
+  }
+
+
+  
+  // Halt communication with the card
+  mfrc522.PICC_HaltA();
+  mfrc522.PCD_StopCrypto1();
+
+  delay(2000);  // Delay for readability
+}
---
+++ leerbloques/leerbloques.ino
@@ -0,0 +1,77 @@
+#include <MFRC522v2.h>
+#include <MFRC522DriverSPI.h>
+//#include <MFRC522DriverI2C.h>
+#include <MFRC522DriverPinSimple.h>
+#include <MFRC522Debug.h>
+
+// Learn more about using SPI/I2C or check the pin assigment for your board: https://github.com/OSSLibraries/Arduino_MFRC522v2#pin-layout
+MFRC522DriverPinSimple ss_pin(5);
+
+MFRC522DriverSPI driver{ss_pin}; // Create SPI driver
+//MFRC522DriverI2C driver{};     // Create I2C driver
+MFRC522 mfrc522{driver};         // Create MFRC522 instance
+
+MFRC522::MIFARE_Key key;
+
+byte blockAddress1 = 10;
+byte blockAddress2 = 9;
+byte bufferblocksize = 18;
+byte blockDataRead1[18];
+byte blockDataRead2[18];
+
+void setup() {
+  Serial.begin(115200);  // Initialize serial communication
+  while (!Serial);       // Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4).
+  
+  mfrc522.PCD_Init();    // Init MFRC522 board.
+ 
+  // Prepare key - all keys are set to FFFFFFFFFFFF at chip delivery from the factory.
+  for (byte i = 0; i < 6; i++) {
+    key.keyByte[i] = 0xFF;
+  }
+}
+
+void loop() {
+  // Check if a new card is present
+  if (!mfrc522.PICC_IsNewCardPresent() || !mfrc522.PICC_ReadCardSerial()) {
+    delay(500);
+    return;
+  }
+
+  // Display card UID
+  Serial.print("\nCard UID: ");
+  MFRC522Debug::PrintUID(Serial, (mfrc522.uid));
+  Serial.println();
+
+  // Authenticate the specified block using KEY_A = 0x60
+  if (mfrc522.PCD_Authenticate(0x60, blockAddress1, &key, &(mfrc522.uid)) != 0) {
+    Serial.println("Authentication failed.");
+    return;
+  }
+
+  //LEER 1 BLOQUE
+  if (mfrc522.MIFARE_Read(blockAddress1, blockDataRead1 , &bufferblocksize) != 0) {
+    Serial.println("Read failed.");
+  } else {
+    for (byte i = 0; i < 16; i++) {
+      Serial.print((char)blockDataRead1[i]);  // Print as character
+    }
+    
+  }
+
+  // LEER 2 BLOQUE
+  if (mfrc522.MIFARE_Read(blockAddress2, blockDataRead2 , &bufferblocksize) != 0) {
+    Serial.println("Read failed.");
+  } else {
+    for (byte i = 0; i < 9; i++) {
+      Serial.print((char)blockDataRead2[i]);  // Print as character
+    }
+
+  }
+  
+  // Halt communication with the card
+  mfrc522.PICC_HaltA();
+  mfrc522.PCD_StopCrypto1();
+
+  delay(2000);  // Delay for readability
+}
